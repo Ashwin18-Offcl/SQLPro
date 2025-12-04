cb504151ac6653ef8f3ac8831a6c94cf65706d5d
ASHWIN <ashwinpanbude1220@gmail.com>
2025-12-04 16:05:21 +0530

    new

---
+++ S2.sql
@@ -0,0 +1,12 @@
+INSERT INTO students VALUES 
+(1, 'Rahul', 20, 85),
+(2, 'Sneha', 22, 90),
+(3, 'Amit', 21, 76),
+(4, 'Pooja', 23, 95);
+CREATE TABLE students (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    marks INT;
+    
+)
